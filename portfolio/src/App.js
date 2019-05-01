f664c32afc8da3c1626b70a1d22161f8487d7a94
mjarraya <mjarraya@student.42.fr>
2019-05-01 15:08:04 +0200

    routing navbar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,12 +2,20 @@ import React from "react";
 import "./App.css";
 import Home from "./components/Home";
 import About from "./components/About";
+import Navbar from "./components/Navbar";
+
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <About />
+      <Navbar />
+      {/* Switch returns only the first matching route */}
+      <Switch>
+        {/* exact so we don't match all paths that start with / */}
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </Switch>
     </div>
   );
 }
